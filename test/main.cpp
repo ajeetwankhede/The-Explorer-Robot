fb7d232cb7e8b94ec4dbc4cc1492b176beb03512
ajeetwankhede <ajeet@umd.edu>
2018-11-27 23:15:48 -0500

    Task: CTS Created main.cpp for testing the node planner and classes.

---
+++ test/main.cpp
@@ -0,0 +1,53 @@
+/************************************************************************
+ MIT License
+ Copyright (c) 2018 Ajeet Wankhede
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ *************************************************************************/
+
+/**
+ *  @copyright MIT License 2018 Ajeet Wankhede
+ *  @file    main.cpp
+ *  @author  Ajeet Wankhede (driver) and Likhita Madiraju (navigator)
+ *  @date    11/27/2018
+ *  @version 1.0
+ *
+ *  @brief UMD ENPM 808X, Final Project, The Explorer Robot
+ *
+ *  @section DESCRIPTION
+ *
+ *  This is a main file for testing ROS node
+ */
+
+#include <ros/ros.h>
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv) {
+ /**
+   * The ros::init() function needs to see argc and argv so that it can perform
+   * any ROS arguments and name remapping that were provided at the command line.
+   * For programmatic remappings you can use a different version of init() which takes
+   * remappings directly, but for most command-line programs, passing argc and argv is
+   * the easiest way to do it.  The third argument to init() is the name of the node.
+   *
+   * You must call one of the versions of ros::init() before using any other
+   * part of the ROS system.
+   */
+    ros::init(argc, argv, "plannerTest");
+
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
